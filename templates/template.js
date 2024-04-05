a74a4cb597ad9c0f411ecd654f53b4ab8614d56e
Vitalij Schwab <schwab.vitalij@gmail.com>
2024-04-05 11:29:06 +0200

    add w3schools

---
+++ templates/template.js
@@ -0,0 +1,14 @@
+// function showCategory(category) {
+//     if (category === 'summary') {
+
+//     }
+//     if(category === 'addTask'){
+
+//     }
+//     if(category === 'board') {
+
+//     }
+//     if (category === 'contacts') {
+
+//     }
+// }
