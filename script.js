b61dc447683e0682e128e1167be01a10b0fe2d71
Vitalij Schwab <schwab.vitalij@gmail.com>
2024-04-07 14:48:43 +0200

    create login and singup

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ async function setItem(key, value) {
 async function getItem(key) {
     const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
     return fetch(url).then(res => res.json()).then(res => {
-        // Verbesserter code
         if (res.data) {
             return res.data.value;
         } throw `Could not find data with key "${key}".`;
@@ -20,4 +19,52 @@ async function getItem(key) {
 
 function goBack() {
     window.history.back();
+}
+
+
+function initLogin() {
+    animationLogo();
+    removePageFromLocalStorage();
+}
+
+
+function animationLogo() {
+    document.querySelector('.logo-container-effect').classList.add('logo-container');
+    document.querySelector('.logo-effect').classList.add('logo');
+}
+
+
+function removePageFromLocalStorage() {
+    if (window.location.pathname === '/index.html') {
+        localStorage.removeItem('page');
+    }
+}
+
+
+function handleSignupOrLogin(hideClass, showClass) {
+    let hideClassContainer = document.getElementById(hideClass);
+    let showClassContainer = document.getElementById(showClass);
+    let signupContainer = document.querySelector('.signup-container');
+    hideClassContainer.style.display = 'none';
+    showClassContainer.style = '';
+    signupContainer.style.opacity = hideClass === 'login' ? '0' : '1';
+}
+
+
+function showCurrentInformationFromLogin() {
+    let comeFromPage = loadPage();
+    document.querySelector('.side-bar-container').style.opacity = comeFromPage === 'index.html' ? '0' : '1';
+}
+
+
+function savePage() {
+    localStorage.setItem('page', JSON.stringify('index.html'));
+}
+
+
+function loadPage() {
+    let loadNotes = localStorage.getItem('page');
+    if (loadNotes) {
+        return JSON.parse(loadNotes);
+    }
 }
