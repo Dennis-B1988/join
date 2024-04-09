3cd9c717939cdbda61b0835fe3be7739f0b193c8
Bogdan <gough@outlook.de>
2024-04-09 07:22:13 +0200

    summary version 1

---
+++ summary/summary.js
@@ -0,0 +1,11 @@
+// function currentDate() {
+//     // JavaScript-Code, um das aktuelle Datum abzurufen und anzuzeigen
+//     let currentDateContainer = document.getElementById("currentDateContainer");
+//     let currentDate = new Date();
+//     let options = { month: 'long', day: '2-digit', year: 'numeric' };
+//     let formattedDate = currentDate.toLocaleDateString('en-US', options);
+//     currentDateContainer.innerHTML = "Heute ist " + formattedDate;
+// }
+
+// // Aufruf der Funktion currentDate, sobald das Skript ausgeführt wird
+// currentDate();
